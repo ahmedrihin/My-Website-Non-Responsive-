c1e03cc1ba55317de5557c3684efaa3ed7535ef8
Md. Mahfuz Ahmed Rihin <81138346+ahmedrihin@users.noreply.github.com>
2021-07-09 18:01:08 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+})
+
+function openNav(){
+    document.getElementById('hamburger-overlay').style.width = '100%';
+}
+function closeNav(){
+    document.getElementById('hamburger-overlay').style.width = '0';
+}
