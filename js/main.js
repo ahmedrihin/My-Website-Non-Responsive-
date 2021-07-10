c0567a74497614b0f1e87415b595865a7de023a5
Md. Mahfuz Ahmed Rihin <81138346+ahmedrihin@users.noreply.github.com>
2021-07-10 23:35:04 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function(){
+    var options = {
+        strings: ['Student', 'Web Designer'],
+        typeSpeed: 40,
+        backSpeed: 40,
+        loop: true
+    };
 
+    var typed = new Typed('.element', options)
+
+
+    $('.aboutStickymenu').waypoint(function(direction){
+        if(direction == 'down'){
+            $('#nav').addClass('sticky');
+        }
+        else{
+            $('#nav').removeClass('sticky');
+        }
+    })
 })
 
-function openNav(){
+
+
+function openNav() {
     document.getElementById('hamburger-overlay').style.width = '100%';
 }
-function closeNav(){
+function closeNav() {
     document.getElementById('hamburger-overlay').style.width = '0';
 }
